4743c4fcd0e4178d0a58a5553a9d082d7827f1e0
Marc Vitalis <marc.vitalis@live.com>
2014-01-03 18:50:18 +0800

    switch to phantomjs for testing

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function(config) {
         port: 9001,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome'],
+        browsers: ['PhantomJS'],
         singleRun: false
     });
 }
